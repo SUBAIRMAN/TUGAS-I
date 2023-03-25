4111c8d1be975b8fff8ad29844239086bd4204f3
SUBAIR <127597855+SUBAIRMAN@users.noreply.github.com>
2023-03-26 01:54:43 +0630

    Add files via upload

---
+++ Asgdll.java
@@ -0,0 +1,15 @@
+package tugas1_13020210310_subairman;
+public class Asgdll {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Kamus */
+float f= 20.0f;
+double fll;
+/* Algoritma */
+fll=10.0f;
+System.out.println ("f : "+f+ "\nf11: "+fll);
+}
+}
